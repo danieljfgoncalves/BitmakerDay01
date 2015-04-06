f48769f6fb863de61a29e209cff28917c7b1e943
Daniel Goncalves <danieljfgoncalves@gmail.com>
2015-04-06 15:05:21 -0400

    Initial Commit

---
+++ BitmakerLabsDay1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BitmakerLabsDay1
+//
+//  Created by Daniel Goncalves on 2015-04-06.
+//  Copyright (c) 2015 Daniel Goncalves. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BitmakerLabsDay1/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BitmakerLabsDay1
+//
+//  Created by Daniel Goncalves on 2015-04-06.
+//  Copyright (c) 2015 Daniel Goncalves. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
