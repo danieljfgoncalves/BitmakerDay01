772b115fdf5edcef4e27a134878134453ac9c798
Daniel Goncalves <danieljfgoncalves@gmail.com>
2015-04-06 15:40:29 -0400

    This is a second commit

--- BitmakerLabsDay1/ViewController.m
+++ BitmakerLabsDay1/ViewController.m
@@ -20,6 +20,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
+// This is a second comment.
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
