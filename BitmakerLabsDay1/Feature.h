275022c1d7e6c835e9a75c8d70f3034ec6f522fd
Daniel Goncalves <danieljfgoncalves@gmail.com>
2015-04-06 15:50:14 -0400

    Added a feature model

---
+++ BitmakerLabsDay1/Feature.h
@@ -0,0 +1,13 @@
+//
+//  Feature.h
+//  BitmakerLabsDay1
+//
+//  Created by Daniel Goncalves on 2015-04-06.
+//  Copyright (c) 2015 Daniel Goncalves. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Feature : NSObject
+
+@end
---
+++ BitmakerLabsDay1/Feature.m
@@ -0,0 +1,13 @@
+//
+//  Feature.m
+//  BitmakerLabsDay1
+//
+//  Created by Daniel Goncalves on 2015-04-06.
+//  Copyright (c) 2015 Daniel Goncalves. All rights reserved.
+//
+
+#import "Feature.h"
+
+@implementation Feature
+
+@end
